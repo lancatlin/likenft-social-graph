a5936868f8ffd9f50254246a2d405486cf78613b
Justin Lin <wancat@wancat.cc>
2022-08-03 10:21:33 +0800

    Auto clean up proxy promise when finish

--- src/proxy.js
+++ src/proxy.js
@@ -8,6 +8,10 @@ function getClass(classId) {
   }
   payloads.set(classId, axios.get(API_PUBLIC+'/likernft/purchase', {
     params: { class_id: classId, }
+  })
+  .then((res) => {
+    payloads.delete(classId);
+    return res;
   }));
   return payloads.get(classId);
 }
